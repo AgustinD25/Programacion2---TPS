9b772bfba5f386c35621efc8a2f70389ce505929
AgustinD25 <agusdiaz025@gmail.com>
2025-09-06 18:33:41 -0300

    Add files via upload

---
+++ Ej_6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej_6;
+
+/**
+ *
+ * @author Agus
+ */
+public class Ej_6 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDireccion: \"Calle Falsa 123\"");
+        
+    
+    }
+    
+}
