a9fc3e6a25ef1329a9e919da3160180ed0d31397
AgustinD25 <agusdiaz025@gmail.com>
2025-09-06 18:32:24 -0300

    Add files via upload

---
+++ Ej_3.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej_3;
+
+/**
+ *
+ * @author Agus
+ */
+public class Ej_3 {
+
+    public static void main(String[] args) {
+        String nombre = "Agustin";
+        int edad = 25;
+        double altura = 1.90;
+        boolean estudiante = true;
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura + "metros");
+        System.out.println("¿Es estudiante?: " + estudiante);
+        
+       
+    }
+    
+}
