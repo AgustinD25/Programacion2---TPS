a9bf5880ebc9996562a9687ebc15806031185678
AgustinD25 <agusdiaz025@gmail.com>
2025-09-26 09:21:54 -0300

    Create Ejercicio2.java

---
+++ Trabajo Práctico N2/Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = scan.nextInt();
+
+        System.out.print("Ingrese el tercer número: ");
+        int num3 = scan.nextInt();
+
+        int mayor = num1; // asumimos que el primero es el mayor
+
+        if (num2 > mayor) {
+            mayor = num2;
+        }
+        if (num3 > mayor) {
+            mayor = num3;
+        }
+
+        System.out.println("El mayor es: " + mayor);
+    }
+}
