e2e537a54cad762ea93824056b39f5b6bb19ffcd
AgustinD25 <agusdiaz025@gmail.com>
2025-09-26 09:33:17 -0300

    Create Ejercicio12.java

---
+++ Trabajo Práctico N2/Ejercicio12.java
@@ -0,0 +1,18 @@
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        double[] precios = {199.99, 299.5, 149.75, 399.0, 89.99};
+
+        System.out.println("Precios originales:");
+        for (double precio : precios) {
+            System.out.println("Precio: $" + precio);
+        }
+
+        // Modificamos el tercer elemento (índice 2)
+        precios[2] = 129.99;
+
+        System.out.println("Precios modificados:");
+        for (double precio : precios) {
+            System.out.println("Precio: $" + precio);
+        }
+    }
+}
